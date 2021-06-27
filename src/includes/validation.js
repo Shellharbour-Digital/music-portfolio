798f859c8ca06c93073d42225e71e6146822fc74
Shellharbour-Digital <hello@shellharbour.digital>
2021-06-27 22:46:06 +1000

    setting up form validation and installed vee validate

---
+++ src/includes/validation.js
@@ -0,0 +1,10 @@
+import {
+  Form as VeeForm, Field as VeeField,
+} from 'vee-validate';
+
+export default {
+  install(app) {
+    app.component('VeeForm', VeeForm);
+    app.component('VeeField', VeeField);
+  },
+};
